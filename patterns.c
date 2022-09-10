4e20f459a13f787ba27518fc6a357b65718510b2
bhabesh kumar baro <111524590+bhabeshbaro@users.noreply.github.com>
2022-09-10 13:32:54 +0530

    Add files via upload

---
+++ patterns.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n ,i,j;
+    printf("enter the number of patterns");
+    scanf("%d",&n);
+
+
+    for (i=1;i<=n;i++)
+    {
+        for(j=0;j<=n;j++)
+
+    {
+        printf("*");
+
+    }
+    printf("\n");
+    }
+
+    return 0;
+
+}   
